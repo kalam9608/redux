636d3dd79dbd43836614bd89b942a2d6198aaea7
mdkalam <arsh9608raja@gmail.com>
2024-09-18 20:01:34 +0530

    rtk with asynk thunk

--- src/AppReduxToolkit.jsx
+++ src/AppReduxToolkit.jsx
@@ -3,27 +3,33 @@ import { useDispatch, useSelector } from "react-redux";
 import AccountComRedux from "./components/AccountComRedux";
 import BonusComRedux from "./components/BonusComRedux";
 import Reward from "./components/Reward";
+import { getUserAccount } from "./slices/accountSlices";
 
 const AppReduxToolkit = () => {
   const dispatch = useDispatch();
 
   const amount = useSelector((state) => state.acount.amount);
   const bonus = useSelector((state) => state.bonus.points);
+  const loading = useSelector((state) => state.acount);
 
-  console.log("am====>", amount);
 
   return (
     <div style={{ maxWidth: "1000px", margin: "auto", alignItems: "center" }}>
       <h1>redux concept app with toolkit</h1>
       <hr />
-      <h2 style={{ color: "green" }}>CurrentAmount:{amount} </h2>
+      {loading.pending ? (
+        <p>loading....</p>
+      ) : (
+        <h2 style={{ color: "green" }}>CurrentAmount:{amount} </h2>
+      )}
       <h2 style={{ color: "green" }}>Bonus:{bonus}</h2>
+      <button onClick={() => dispatch(getUserAccount(1))}>get user </button>
       <hr />
-      <AccountComRedux/>
+      <AccountComRedux />
       <hr />
-      <BonusComRedux/>
+      <BonusComRedux />
       <hr />
-<Reward/>
+      <Reward />
     </div>
   );
 };
--- src/slices/accountSlices.js
+++ src/slices/accountSlices.js
@@ -1,23 +1,45 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   amount: 2,
 };
+
+export const getUserAccount = createAsyncThunk(
+  "account/getUser",
+  async (userId, thunkAPI) => {
+    console.log("userid===>",userId)
+    const { data } = await axios.get(`http://localhost:3000/account/${userId}`);
+    console.log("data==>",data)
+    return data.amount;
+  }
+);
 
 export const accountSlices = createSlice({
   name: "account",
   initialState,
   reducers: {
     increment: (state) => {
-        console.log("calling",state)
+      console.log("calling", state);
       state.amount += 1;
     },
     decrement: (state) => {
       state.amount -= 1;
     },
     incrementByAmount: (state, action) => {
-      state.amount  += action.payload;
+      state.amount += action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getUserAccount.fulfilled, (state, action) => {
+      state.amount += action.payload;
+      state.pending = false;
+    }).addCase(getUserAccount.pending, (state, action) => {
+      state.pending = true;
+    }).addCase(getUserAccount.rejected, (state, action) => {
+        console.log("stat===>",action,state)
+      state.pending = false;
+    });
   },
 });
 
